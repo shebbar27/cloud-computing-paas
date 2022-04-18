34c2cba91e7eddf4ce9d566f617aee395f29f004
Sunaada Hebbar <shebbarm@asu.edu>
2022-04-17 22:18:27 -0700

    modify run.sh

--- train_face_recognition_model/run.sh
+++ train_face_recognition_model/run.sh
@@ -1,2 +1,2 @@
-python3 train_face_recognition.py --data_dir "data/test_me" --num_epochs 3
+python3 train_face_recognition.py --data_dir "data/processed-images" --num_epochs 3
 python3 eval_face_recognition.py --img_path "data/processed-images/val/Anup/My_photo_Date_30.png"
